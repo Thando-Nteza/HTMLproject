9e9364031eea631bd73aa71ea5340c89407c567b
Thando-Nteza <59652115+Thando-Nteza@users.noreply.github.com>
2020-02-13 14:46:04 +0200

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,16 @@
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDy5TnNUFJCsq9ylg9RnErG_zaMrh0hjPs",
+    authDomain: "thando-58148.firebaseapp.com",
+    databaseURL: "https://thando-58148.firebaseio.com",
+    projectId: "thando-58148",
+    storageBucket: "thando-58148.appspot.com",
+    messagingSenderId: "1058088025481",
+    appId: "1:1058088025481:web:3bb33c2a813c5ff9b57558",
+    measurementId: "G-6M9LPLNNWJ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
